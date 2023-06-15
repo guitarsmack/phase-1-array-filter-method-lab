df29f484c8e49f243f515f6a2bc98ce8bef8c799
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-15 09:07:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array,names){
+    const list = array.filter(item => item.toUpperCase() === names.toUpperCase())
+    return list
+}
+
+function fuzzyMatch(array,letters){
+    const list = array.filter(item => item.slice(0,2) === letters.slice(0,2))
+    return list
+}
+
+function matchName(array,string){
+    const list = array.filter(item => item.name === string)
+    return list
+}
